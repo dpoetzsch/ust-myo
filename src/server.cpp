56d0db0ba0bb407f7c5d4f305e3da90a175b0686
David Poetzsch-Heffter <davidpoetzsch@web.de>
2016-06-12 18:10:17 +0200

    prepared GrabServer implementation for our purpose

--- src/server.cpp
+++ src/server.cpp
@@ -8,33 +8,95 @@
 #include <ctime>
 #include <iostream>
 #include <string>
+#include <boost/algorithm/string.hpp>
+#include <boost/lexical_cast.hpp>
 #include <boost/asio.hpp>
+#include <boost/thread/thread.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+
+#ifdef LINUX
 #include <unistd.h>
+#endif
+#ifdef WINDOWS
+#include <windows.h>
+#endif
 
 using boost::asio::ip::tcp;
-
-const int SERVER_PORT = 2000;
 
 std::string make_daytime_string() {
   using namespace std; // For time_t, time and ctime;
   time_t now = time(0);
-  return ctime(&now);
+  std::string s = ctime(&now);
+  boost::algorithm::trim_right(s);
+  return s;
 }
+
+class GrabServer {
+public:
+  static const int SERVER_PORT = 2000;
+
+  GrabServer()
+      : acceptor(io_service, tcp::endpoint(tcp::v4(), SERVER_PORT)),
+        socket(NULL) {}
+  ~GrabServer() {
+    if (socket != NULL) {
+      delete socket;
+    }
+  }
+
+  void acceptConnection() {
+    std::cout << "Waiting for connection... ";
+    std::cout.flush();
+    if (socket != NULL) {
+      delete socket;
+    }
+    socket = new tcp::socket(io_service);
+    acceptor.accept(*socket);
+    std::cout << "Connected.\n";
+  }
+
+  /**
+   * @return true if sending was successful, false if the connection was closed.
+   */
+  bool signalGrab(int likelyhood) {
+    return sendMessage("{ \"grabbed\": \"" + boost::lexical_cast<std::string>(likelyhood) + "\", \"time\": \"" + make_daytime_string() + "\" }");
+  }
+
+private:
+  /**
+   * @return true if sending was successful, false if the connection was closed.
+   */
+  bool sendMessage(std::string msg) {
+    if ((socket == NULL) || !socket->is_open()) {
+      std::cout << "Connection was closed.\n";
+      return false;
+    } else {
+      std::cout << "Sending message: '" << msg << "'\n";
+      boost::system::error_code error;
+      boost::asio::write(*socket, boost::asio::buffer(msg), error);
+      if (error != 0) {
+        std::cout << "Connection was closed.\n";
+        return false;
+      } else {
+        return true;
+      }
+    }
+  }
+
+  boost::asio::io_service io_service;
+  tcp::acceptor acceptor;
+  tcp::socket *socket;
+};
 
 int main() {
   try {
-    boost::asio::io_service io_service;
-
-    tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v4(), SERVER_PORT));
+    GrabServer server;
 
     for (;;) {
-      tcp::socket socket(io_service);
-      acceptor.accept(socket);
+      server.acceptConnection();
 
-      while (socket.is_open()) {
-        boost::system::error_code ignored_error;
-        boost::asio::write(socket, boost::asio::buffer(make_daytime_string()), ignored_error);
-        usleep(1000000); // sleep 1 sec
+      while (server.signalGrab(85)) {
+        boost::this_thread::sleep(boost::posix_time::millisec(1000));
       }
     }
   } catch (std::exception &e) {
