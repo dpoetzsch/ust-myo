bc97e39e4645b165ded32928000f744cb6e60c3d
David Poetzsch-Heffter <davidpoetzsch@web.de>
2016-06-14 01:32:15 +0200

    updated c# receiver to recognize the extremity sent by the server

--- csharp-client/client.cs
+++ csharp-client/client.cs
@@ -6,12 +6,19 @@ using System.Net.Sockets;
 using System.Threading;
 using Newtonsoft.Json;
 
+enum Extremity {
+    HAND_LEFT,
+    HAND_RIGHT,
+    FOOT_LEFT,
+    FOOT_RIGHT
+}
+
 class GrabClient {
-    private Action<int> onGrabbed;
+    private Action<Extremity, int> onGrabbed;
     private TcpClient tcpclnt;
     private Thread listenThread;
 
-    public GrabClient(Action<int> onGrabbed) {
+    public GrabClient(Action<Extremity, int> onGrabbed) {
         this.onGrabbed = onGrabbed;
     }
 
@@ -29,6 +36,21 @@ class GrabClient {
         listenThread.Start();
     }
 
+    static Extremity fromInt(int extremity) {
+        switch (extremity) {
+        case 1:
+            return Extremity.HAND_LEFT;
+        case 2:
+            return Extremity.HAND_RIGHT;
+        case 3:
+            return Extremity.FOOT_LEFT;
+        case 4:
+            return Extremity.FOOT_RIGHT;
+        default:
+            throw new ArgumentException();
+        }
+    }
+
     private void Run() {
         using (StreamReader reader = new StreamReader(tcpclnt.GetStream())) {
             while (true) {
@@ -37,7 +59,8 @@ class GrabClient {
 
                 // TODO if needed we should check if the timing is fine using the stuff.time
                 int likelyhood = stuff.likelyhood;
-                onGrabbed(likelyhood);
+                int extremity = stuff.extremity;
+                onGrabbed(fromInt(extremity), likelyhood);
             }
         }
     }
@@ -49,13 +72,25 @@ class GrabClient {
 
 
 class MainClass {
-    public static void OnGrabbed(int likelyhood) {
+    public static void OnGrabbed(Extremity extremity, int likelyhood) {
+        switch (extremity) {
+        case Extremity.HAND_LEFT:
+            Console.Write("Left hand: ");
+            break;
+        case Extremity.HAND_RIGHT:
+            Console.Write("Right hand: ");
+            break;
+        case Extremity.FOOT_LEFT:
+            Console.Write("Left foot: ");
+            break;
+        case Extremity.FOOT_RIGHT:
+            Console.Write("Right foot: ");
+            break;
+        }
         Console.WriteLine(likelyhood);
     }
 
     public static void Main() {
-        MainClass m = new MainClass();
-
         GrabClient c = new GrabClient(OnGrabbed);
         c.Connect();
         c.Listen();
